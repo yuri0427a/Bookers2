ffea9e77d4cc2a15e8cdcc083cc276f2319b3d8c
yuri0427a <yuri0427ita@gmail.com>
2020-01-30 07:14:00 +0000

    [Add]bookモデル

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   validates :name,uniqueness: true
+  validates :name, length: { minimum: 1, maximum: 20}
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :trackable, :validatable
  # deviseでemailを不必要にする
@@ -11,4 +12,6 @@ class User < ApplicationRecord
     def email_changed?
      false
     end
+
+    has_many :book, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_27_102909) do
+ActiveRecord::Schema.define(version: 2020_01_29_064250) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
